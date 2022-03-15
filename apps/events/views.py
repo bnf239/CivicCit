0b4069181e4927490cb16c4a72bbefe2476d8465
bnf239 <bnf239@nyu.edu>
2022-03-14 23:48:40 -0400

    sample events page

--- apps/events/views.py
+++ apps/events/views.py
@@ -10,15 +10,29 @@ from bs4 import BeautifulSoup
  
 def events_view(request):
     # complete logic here
-    url = "https://www.imdb.com/chart/moviemeter/"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    table = soup.find('table',  {'class': 'chart full-width'})
-    rows = table.find_all('tr')
-    movies = []
-    for row in rows:
-        image = row.find('img')
-        if image:
-            movies.append(image['alt'])
-    return render(request, "events/events.html", {'movies': movies})
+    # url = "https://www.imdb.com/chart/moviemeter/"
+    # response = requests.get(url)
+    # soup = BeautifulSoup(response.content, "html.parser")
+    # table = soup.find('table',  {'class': 'chart full-width'})
+    # rows = table.find_all('tr')
+    # movies = []
+    # for row in rows:
+    #     image = row.find('img')
+    #     if image:
+    #         movies.append(image['alt'])
+    headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+    url = 'https://www.meetup.com/find/events/?allMeetups=true&radius=25&userFreeform=New York, NY&mcId=c10001&mcName=New York, NY&categoryId=604'
+    response=requests.get(url,headers=headers)
+    soup=BeautifulSoup(response.content,'html')
+    events = []
+    for i in range(len(soup.find("div", { "class" : "max-w-narrow" }).find("div", { "class" : "max-w-narrow" }).contents)):
+        try:
+            print(soup.find("div", { "class" : "max-w-narrow" }).find("div", { "class" : "max-w-narrow" }).contents[i].select('p')[0].get_text())
+            events.append(soup.find("div", { "class" : "max-w-narrow" }).find("div", { "class" : "max-w-narrow" }).contents[i].select('p')[0].get_text())
+        except:
+            pass
+            #print(soup.find("div", { "class" : "max-w-narrow" }).find("div", { "class" : "max-w-narrow" }).contents[i].select())
+    
+
+    return render(request, "events/events.html", {'events': events})
  
