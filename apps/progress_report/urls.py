307ad96c8a3813e3ec1af37475f2dfbbe51e7c07
Monique Aboumrad <mas1790@nyu.edu>
2022-04-25 04:18:46 -0400

    monique's part nearly finished

--- apps/progress_report/urls.py
+++ apps/progress_report/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, re_path, include
 from apps.home import views
-from .views import progress_report_view, number_of_events_registered_for
+from .views import download_file, progress_report_view, number_of_events_registered_for
 
 
 app_name = "progress_report"
 
 urlpatterns = [
 
-    path('progress_report/', progress_report_view, name="progress_report"),
+    path('progress_report/', progress_report_view, name="progress_report"), 
+    path('/progress_report/', download_file, name = "download_file")
     #path('infohubtest/', infohubtest_view, name='infohubtest'),
     #path('infohubtest2/', infohubtest2_view, name='infohubtest2'),
    # path('infohubtest3/', infohubtest3_view, name='infohubtest3'),
--- apps/progress_report/views.py
+++ apps/progress_report/views.py
@@ -1,12 +1,20 @@
 from unicodedata import category
 from django import template
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseRedirect
+
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
+
+import mimetypes
+import os 
+
+
 from django.template import loader
 from django.shortcuts import render, redirect
 from django.urls import reverse
 import requests
 from bs4 import BeautifulSoup
+
+
 
 
 # from .forms import EventForm
@@ -78,6 +86,25 @@ def number_of_quizzes(request):
 def quiz_info(request):
     quiz = list(QuizCategoryModel.objects.all().filter(user_id=request.user.id))
     return quiz
+
+def download_file(request):
+ # Define Django project base directory
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # Define text file name
+    filename = 'test.txt'
+    # Define the full file path
+    filepath = BASE_DIR + '/downloadapp/Files/' + filename
+    # Open the file for reading content
+    path = open(filepath, 'r')
+    # Set the mime type
+    mime_type, _ = mimetypes.guess_type(filepath)
+    # Set the return value of the HttpResponse
+    response = HttpResponse(path, content_type=mime_type)
+    # Set the HTTP header for sending to browser
+    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    # Return the response value
+    return response
+
 
 
    
