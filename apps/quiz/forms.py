222469fee3625a298c5ade4edae6cc4bba7557f0
bnf239 <bnf239@nyu.edu>
2022-04-26 13:16:26 -0400

    updated quiz/forms.py

--- apps/quiz/forms.py
+++ apps/quiz/forms.py
@@ -3,10 +3,82 @@ from django import forms
 
 class PQuizForm(forms.Form):
 
-    try:
+    if(len(list(QuesModel.objects.filter(category='P')))!=0):
         listQuestions = list(QuesModel.objects.filter(category='P'))  
-    except:
-        listQuestions = []
+    else:
+        listQuestions = [ 
+            {"question": "Why is it required for all American citizens to obey the law?", 
+            "op1":"to ensure citizen\'s life, liberty and property are protected",
+            "op2": "to ensure all Americans are treated equally",
+            "op3": "to ensure an executive order is approved by Congress",
+            "op4": "to ensure enough funds are collected in speeding tickets to fund the police", 
+            "ans":"to ensure citizen\'s life, liberty and property are protected"},
+            {"question":"Voting in the US is legally a:",
+            "op1":"right", 
+            "op2":"obligation",
+            "op3":"duty", 
+            "op4":"burden", 
+            "ans":"right"},
+            {"question":"When was the last time the draft was enacted in the United States?", 
+            "op1":"The Vietnam War",
+            "op2":"The Persian Gulf War", 
+            "op3":"Korean War", 
+            "op4":"World War II", 
+            "ans":"The Vietnam War"},
+            {"question":"Which of the following is legally permitted?",  
+            "op1":"men not registering for the Selective Service at age 18", 
+            "op2":"dropping out of high school in Missouri when you are 15 years old", 
+            "op3":"not voting", 
+            "op4":"tax evasion", 
+            "ans":"not voting"},
+            {
+            "question":"The right to vote best relates to which Constitutional principle?",
+            "op1":"judicidal review",
+            "op2":"checks and balances",
+            "op3":"separation of powers",
+            "op4":"popular sovereignty",
+            "ans":"popular sovereignty"
+            },
+            {
+            "question":"What does the Constitution do?",
+            "op1":"sets up the government",
+            "op2":"defines the government",
+            "op3":"protects the basic rights of Americans",
+            "op4":"all of the above",
+            "ans":"all of the above"
+            },
+            {
+            "question":"Which group actually picks the Presidential winner based on the popular vote?",
+            "op1":"the governors of each state",
+            "op2":"the US senate",
+            "op3":"the US house of representatives",
+            "op4":"the Electoral College",
+            "ans":"the Electoral College"
+            },
+            {
+            "question":"Which of the following is not one of the ways in which the media affects politics?",
+            "op1":"by providing information on both parties",
+            "op2":"by contributing to election campaign funds",
+            "op3":"by focusing public attention on particular issues",
+            "op4":"by providing in-depth coverage of national news",
+            "ans":"by contributing to election campaign funds"
+            },
+            {
+            "question":"Which best describes the main objective of a lobbyist?",
+            "op1":"to raise campaign funds for political parties",
+            "op2":"to influence public decision-making for the common good",
+            "op3":"to influence state legislators or members of Congress on issues",
+            "op4":"to advise political candidates on how to manage their election campaigns",
+            "ans":"to influence state legislators or members of Congress on issues"
+            },
+            {
+            "question":"The validity of public opinion polls may be affected by several factors including:",
+            "op1":"excessing polling",
+            "op2":"poll overrepresentation of political culture factors such as liberty and civic duty",
+            "op3":"overrepresentation of the views of the elite",
+            "op4":"the wording of the questions of the poll",
+            "ans":"the wording of the questions of the poll"
+            }]
     
     try:
         CHOICES_1 = (
@@ -17,13 +89,14 @@ class PQuizForm(forms.Form):
         )
         answers1 = forms.CharField(label=listQuestions[0].question, widget=forms.Select(choices = CHOICES_1), required=True)
     except:
+        print(listQuestions)
         CHOICES_1 = (
-            ("A", "A"),
-            ("B", "B"),
-            ("C", "C"),
-            ("D", "D"),
+             (listQuestions[0]["op1"],listQuestions[0]["op1"]),
+            (listQuestions[0]["op2"],listQuestions[0]["op2"]),
+           (listQuestions[0]["op3"],listQuestions[0]["op3"]),
+           (listQuestions[0]["op4"],listQuestions[0]["op4"]),
         )
-        answers1 = forms.CharField(label="test question", widget=forms.Select(choices = CHOICES_1), required=True)
+        answers1 = forms.CharField(label=listQuestions[0]["question"], widget=forms.Select(choices = CHOICES_1), required=True)
     
     try:
         CHOICES_2 = (
